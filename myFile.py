78ddb962221bc0dcbdeba955d501e8ba5260fc19
kamryn0906 <57299220+kamryn0906@users.noreply.github.com>
2020-06-05 13:44:42 -0400

    update python function with merge conflict

--- myFile.py
+++ myFile.py
@@ -4,6 +4,9 @@ print('Hi from another dev')
 
 def hi_there():
     print("hi")
-
+    
+def hi_there():
+    print("update python function with merge conflict")
+    
 def my function():
     return "hi"
